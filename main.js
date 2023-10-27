8b14d9049bded9136c148ac0d71906398269162a
mariareis0 <106742987+mariareis0@users.noreply.github.com>
2023-10-27 09:00:59 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-documento.querySelector('.tecla_pom');
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play('');
+}
+
+const listaDeTeclas = documento.querySelectorAll('.tecla');
+
+for(contador = 0; contador < listaDeTeclas.length++){
+
+    const instrumento = listaDeTeclas[contador].classList[1];
+
+    const tecla = listaDeTeclas[contador];
+
+const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function(){
+        tocaSom(idAudio);
+    };
+
+}
+
+listaDeTeclas[1].onclick = tocaSomClap;
+listaDeTeclas[2].onclick = tocaSomTim;
+listaDeTeclas[3].onclick = tocaSomPuff;
